f4c656c3c9c03dcfd3bfda692f73c84db0aae5b8
Geoffrey Mattie <gmattie@gmail.com>
2023-12-09 20:38:49 -0500

    port update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require("path")
 const express = require("express")
 const { WebSocketServer } = require("ws")
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app
